466f4d532a3dda95eedf40cf16d84e0adf979717
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 22:30:31 +0000

    uploaded solution for 1075

--- SQL_Top_50_Q/Basic Aggregate Functions/Project Employees I/MsSQL_solution.sql
+++ SQL_Top_50_Q/Basic Aggregate Functions/Project Employees I/MsSQL_solution.sql
@@ -1,1 +1,7 @@
-# MsSQL solution for Project Employees I
+-- MsSQL solution for Project Employees I
+
+SELECT project_id, 
+    ROUND(AVG(CAST(e.experience_years AS DECIMAL)), 2) AS average_years 
+FROM Project p LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
--- SQL_Top_50_Q/Basic Aggregate Functions/Project Employees I/Pandas_solution.py
+++ SQL_Top_50_Q/Basic Aggregate Functions/Project Employees I/Pandas_solution.py
@@ -1,1 +1,6 @@
 # Pandas solution for Project Employees I
+
+import pandas as pd
+
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+    return project.merge(employee, how="left", on="employee_id").groupby("project_id")['experience_years'].mean().round(2).reset_index(name='average_years')
