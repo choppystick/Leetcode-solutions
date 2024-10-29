38ac78706cc3f604d05c817d5f1113fa2446d3d2
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:20:21 -0400

    Update MySQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Managers with at Least 5 Direct Reports/MySQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Managers with at Least 5 Direct Reports/MySQL_solution.sql
@@ -1,1 +1,8 @@
 -- MySQL solution for Managers with at Least 5 Direct Reports
+
+select E1.name FROM Employee E1 
+INNER JOIN (
+    select managerId FROM Employee
+    GROUP BY managerId
+    having count(*) >= 5
+) as F on E1.id = F.managerId
