3ac0ecbf6bd34e7c09960123fcc064694997dc07
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:19:58 -0400

    Update Pandas_solution.py

--- SQL_Top_50_Q/Basic Joins/Managers with at Least 5 Direct Reports/Pandas_solution.py
+++ SQL_Top_50_Q/Basic Joins/Managers with at Least 5 Direct Reports/Pandas_solution.py
@@ -1,1 +1,7 @@
 # Pandas solution for Managers with at Least 5 Direct Reports
+
+import pandas as pd
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    manager_id = employee["managerId"].value_counts().reset_index(name="count")
+    return employee.merge(manager_id[manager_id["count"] >= 5], how="inner", left_on="id", right_on="managerId")[["name"]]
