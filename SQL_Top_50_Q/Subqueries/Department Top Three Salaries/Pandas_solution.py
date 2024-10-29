1b5e3c4c7206aeb1f3e0ac5b831e81d9f162b6a1
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 21:48:17 +0000

    Uploaded panda solution

--- SQL_Top_50_Q/Subqueries/Department Top Three Salaries/Pandas_solution.py
+++ SQL_Top_50_Q/Subqueries/Department Top Three Salaries/Pandas_solution.py
@@ -1,1 +1,26 @@
 # Pandas solution for Department Top Three Salaries
+import pandas as pd
+
+def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    merged_df = employee.merge(
+        department,
+        left_on='departmentId',
+        right_on='id',
+        suffixes=('_employee', '_department')
+    )
+
+    def get_top_3_unique_salaries(group):
+        top_3_salaries = sorted(group['salary'].unique(), reverse=True)[:3]
+        return group[group['salary'].isin(top_3_salaries)]
+    
+    result = (merged_df.groupby('name_department', as_index=False)
+              .apply(get_top_3_unique_salaries)
+              .reset_index(drop=True)
+              .rename(columns={
+                  'name_employee': 'Employee',
+                  'name_department': 'Department',
+                  'salary': 'Salary'
+              })[['Department', 'Employee', 'Salary']]
+             )
+    
+    return result
