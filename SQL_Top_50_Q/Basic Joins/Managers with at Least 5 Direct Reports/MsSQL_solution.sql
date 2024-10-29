b5be9dfbf026411353661e58442df661dd300575
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:20:43 -0400

    Update MsSQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Managers with at Least 5 Direct Reports/MsSQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Managers with at Least 5 Direct Reports/MsSQL_solution.sql
@@ -1,1 +1,10 @@
-# MsSQL solution for Managers with at Least 5 Direct Reports
+-- MsSQL solution for Managers with at Least 5 Direct Reports
+
+SELECT E1.name 
+FROM Employee E1 
+INNER JOIN (
+    SELECT managerId 
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) AS F ON E1.id = F.managerId;
