5b1d8ed4d01d02e44f744cfb92e568a49cd867e1
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 11:54:19 -0400

    Update Pandas_solution.py

--- SQL_Top_50_Q/Select/Find Customer Referee/Pandas_solution.py
+++ SQL_Top_50_Q/Select/Find Customer Referee/Pandas_solution.py
@@ -1,1 +1,6 @@
 # Pandas solution for Find Customer Referee
+
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    return customer.loc[((customer.referee_id.isna()) | (customer.referee_id != 2)), ["name"]]
