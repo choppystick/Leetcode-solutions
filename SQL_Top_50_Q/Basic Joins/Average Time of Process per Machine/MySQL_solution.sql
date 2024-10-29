dbf442ca782f57ef41579454a32e08d5ad1ebe97
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:17:03 -0400

    Update MySQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Average Time of Process per Machine/MySQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Average Time of Process per Machine/MySQL_solution.sql
@@ -1,1 +1,12 @@
 -- MySQL solution for Average Time of Process per Machine
+
+select start.machine_id, round(avg(end.timestamp - start.timestamp), 3) processing_time
+from Activity start JOIN Activity end
+    on start.machine_id = end.machine_id 
+    and start.process_id = end.process_id 
+    and start.activity_type = "start"
+    and end.activity_type = "end"
+group by start.machine_id
+
+
+
