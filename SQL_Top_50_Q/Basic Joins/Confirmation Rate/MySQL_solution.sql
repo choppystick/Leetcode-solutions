183d092614d0063e4599a7ba7e2cc1a344752cde
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:09:23 -0400

    Update MySQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Confirmation Rate/MySQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Confirmation Rate/MySQL_solution.sql
@@ -1,1 +1,6 @@
 -- MySQL solution for Confirmation Rate
+
+SELECT sig.user_id, round(coalesce(avg(action = 'confirmed'), 0) , 2) confirmation_rate
+FROM Signups sig LEFT JOIN Confirmations con
+on sig.user_id = con.user_id
+GROUP BY sig.user_id
