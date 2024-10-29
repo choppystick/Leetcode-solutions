93b0011c200b6e6278228a069a9b8e628d5a5383
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:26:44 -0400

    Update MsSQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Customer Who Visited but Did Not Make Any Transactions/MsSQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Customer Who Visited but Did Not Make Any Transactions/MsSQL_solution.sql
@@ -1,1 +1,10 @@
-# MsSQL solution for Customer Who Visited but Did Not Make Any Transactions
+-- MsSQL solution for Customer Who Visited but Did Not Make Any Transactions
+
+SELECT 
+    v.customer_id, 
+    COUNT(*) AS count_no_trans 
+FROM Visits v 
+LEFT JOIN Transactions t 
+    ON v.visit_id = t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY v.customer_id;
