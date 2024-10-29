6ef12f0e541603cfff86fae56688b5e58cb1057a
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 11:50:58 -0400

    Update MsSQL_solution.sql

--- SQL_Top_50_Q/Select/Recyclable and Low Fat Products/MsSQL_solution.sql
+++ SQL_Top_50_Q/Select/Recyclable and Low Fat Products/MsSQL_solution.sql
@@ -1,1 +1,6 @@
-# MsSQL solution for Recyclable and Low Fat Products
+-- MsSQL solution for Recyclable and Low Fat Products
+  
+SELECT product_id 
+FROM Products
+WHERE low_fats = 'Y' 
+AND recyclable = 'Y';
