762e61fd9326481a1de9e425fcb071b415b6f35a
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:16:39 -0400

    Update Pandas_solution.py

--- SQL_Top_50_Q/Basic Joins/Average Time of Process per Machine/Pandas_solution.py
+++ SQL_Top_50_Q/Basic Joins/Average Time of Process per Machine/Pandas_solution.py
@@ -1,1 +1,11 @@
 # Pandas solution for Average Time of Process per Machine
+
+import pandas as pd
+
+def get_average_time(activity: pd.DataFrame) -> pd.DataFrame:
+    activity.sort_values(by=["machine_id", "process_id", "activity_type"], ascending=[True, True, False], inplace=True)
+    diff = activity.timestamp.diff()
+    machine_id = activity[activity.activity_type=="end"]["machine_id"]
+    return (pd.DataFrame({"machine_id": machine_id, "processing_time":diff}).groupby("machine_id").mean().round(3).reset_index())
+    
+    
