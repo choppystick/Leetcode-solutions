3c178c8dcaccf15cf1d7f56e51b73fead18a2a5e
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 11:57:44 -0400

    Update Pandas_solution.py

--- SQL_Top_50_Q/Select/Article Views I/Pandas_solution.py
+++ SQL_Top_50_Q/Select/Article Views I/Pandas_solution.py
@@ -1,1 +1,7 @@
 # Pandas solution for Article Views I
+
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    authors = views[views.author_id == views.viewer_id]["author_id"]
+    return pd.DataFrame({"id":sorted(authors.unique())})
