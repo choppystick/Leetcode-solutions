a4163adfedc31c1fe1bd209f57a9154a202977e8
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 11:50:38 -0400

    Update Pandas_solution.py

--- SQL_Top_50_Q/Select/Recyclable and Low Fat Products/Pandas_solution.py
+++ SQL_Top_50_Q/Select/Recyclable and Low Fat Products/Pandas_solution.py
@@ -1,1 +1,6 @@
 # Pandas solution for Recyclable and Low Fat Products
+
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products.loc[(products.low_fats == "Y") & (products.recyclable == "Y"), ["product_id"]]
