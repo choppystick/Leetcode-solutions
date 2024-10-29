14a4cf6bcb9a5bd3ce1799cbcf10f4deac058948
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:29:36 -0400

    Update MsSQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Product Sales Analysis I/MsSQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Product Sales Analysis I/MsSQL_solution.sql
@@ -1,1 +1,8 @@
-# MsSQL solution for Product Sales Analysis I
+-- MsSQL solution for Product Sales Analysis I
+
+SELECT 
+    p.product_name, 
+    s.year, 
+    s.price
+FROM Sales s 
+LEFT JOIN Product p ON p.product_id = s.product_id;
