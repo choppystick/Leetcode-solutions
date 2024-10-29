0974f529aff38e45468897594f75c08095bcb423
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:08:50 -0400

    Update MsSQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Confirmation Rate/MsSQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Confirmation Rate/MsSQL_solution.sql
@@ -1,1 +1,8 @@
-# MsSQL solution for Confirmation Rate
+-- MsSQL solution for Confirmation Rate
+
+SELECT 
+    sig.user_id, 
+    ROUND(COALESCE(AVG(CASE WHEN action = 'confirmed' THEN 1.0 ELSE 0.0 END), 0), 2) AS confirmation_rate
+FROM Signups sig 
+LEFT JOIN Confirmations con ON sig.user_id = con.user_id
+GROUP BY sig.user_id;
