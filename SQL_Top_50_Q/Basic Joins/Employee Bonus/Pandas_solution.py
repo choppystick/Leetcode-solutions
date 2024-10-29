67f31e9385e5236587f54830ed202bfbe3c6c709
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:27:55 -0400

    Update Pandas_solution.py

--- SQL_Top_50_Q/Basic Joins/Employee Bonus/Pandas_solution.py
+++ SQL_Top_50_Q/Basic Joins/Employee Bonus/Pandas_solution.py
@@ -1,1 +1,7 @@
 # Pandas solution for Employee Bonus
+
+import pandas as pd
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    df = employee.merge(bonus, on="empId", how="left")
+    return df.loc[(df.bonus < 1000) | (df.bonus.isna())][["name", "bonus"]]
