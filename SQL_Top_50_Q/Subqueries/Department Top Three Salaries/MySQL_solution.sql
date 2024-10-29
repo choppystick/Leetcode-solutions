a9c7fe49309af630b1c9270d9ab16e32ebedab36
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 21:32:38 +0000

    Uploaded solution.

--- SQL_Top_50_Q/Subqueries/Department Top Three Salaries/MySQL_solution.sql
+++ SQL_Top_50_Q/Subqueries/Department Top Three Salaries/MySQL_solution.sql
@@ -1,1 +1,10 @@
 -- MySQL solution for Department Top Three Salaries
+
+
+SELECT d.name Department, E1.name Employee, E1.salary Salary
+FROM Employee E1 LEFT JOIN Department d ON E1.departmentId = d.id
+WHERE (
+SELECT COUNT(DISTINCT E2.salary)
+FROM Employee E2
+WHERE E1.departmentId = E2.departmentId AND E1.salary <= E2.salary 
+) <= 3
