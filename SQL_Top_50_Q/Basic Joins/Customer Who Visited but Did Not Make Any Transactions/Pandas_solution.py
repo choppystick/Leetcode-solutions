522da46f3aedba80b5b44922448f4eadbc153694
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:21:15 -0400

    Update Pandas_solution.py

--- SQL_Top_50_Q/Basic Joins/Customer Who Visited but Did Not Make Any Transactions/Pandas_solution.py
+++ SQL_Top_50_Q/Basic Joins/Customer Who Visited but Did Not Make Any Transactions/Pandas_solution.py
@@ -1,1 +1,7 @@
 # Pandas solution for Customer Who Visited but Did Not Make Any Transactions
+
+import pandas as pd
+
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    merge = visits.merge(transactions, on="visit_id", how="left")
+    return merge[merge.transaction_id.isna()].groupby("customer_id").size().reset_index(name="count_no_trans")
