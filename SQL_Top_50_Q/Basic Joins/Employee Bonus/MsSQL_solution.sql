17c35e7a7048250821edb53176eb30c720252cbe
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:28:38 -0400

    Update MsSQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Employee Bonus/MsSQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Employee Bonus/MsSQL_solution.sql
@@ -1,1 +1,9 @@
-# MsSQL solution for Employee Bonus
+-- MsSQL solution for Employee Bonus
+
+SELECT 
+    E.name, 
+    B.bonus
+FROM Employee E 
+LEFT JOIN Bonus B ON E.empId = B.empId
+WHERE B.bonus < 1000 
+    OR B.bonus IS NULL;
