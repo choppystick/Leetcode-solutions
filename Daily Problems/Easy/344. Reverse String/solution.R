f00b4208e539dfe5102b3a762dcca9f5403214d6
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 21:57:52 +0000

    uploaded solution for 344

--- Daily Problems/Easy/344. Reverse String/solution.R
+++ Daily Problems/Easy/344. Reverse String/solution.R
@@ -1,1 +1,23 @@
 # R solution for 344. Reverse String
+
+reverse_string <- function(s){
+  left = 1
+  right = length(s)
+  
+  while (left < right){
+    temp <- s[left]
+    s[left] <- s[right]
+    s[right] <- temp
+    
+    left = left + 1
+    right = right - 1
+  }
+  
+  return(s)
+}
+
+test1 <- c("h", "e", "l", "l", "o")
+test2 <- c("H", "a", "n", "n", "a", "h")
+
+reverse_string(test1)
+reverse_string(test2)
--- Daily Problems/Easy/344. Reverse String/solution.py
+++ Daily Problems/Easy/344. Reverse String/solution.py
@@ -1,1 +1,7 @@
 # Python solution for 344. Reverse String
+
+def reverseString(self, s: List[str]) -> None:
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    for i in range(len(s) >> 1): s[i], s[~i] = s[~i], s[i]
