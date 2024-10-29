5f182b9bd1d122ede8abbbf61ad7c4476d65eede
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:19:25 -0400

    Update MsSQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Average Time of Process per Machine/MsSQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Average Time of Process per Machine/MsSQL_solution.sql
@@ -1,1 +1,12 @@
-# MsSQL solution for Average Time of Process per Machine
+-- MsSQL solution for Average Time of Process per Machine
+
+SELECT 
+    s.machine_id, 
+    ROUND(AVG(CAST(e.timestamp - s.timestamp AS FLOAT)), 3) AS processing_time
+FROM Activity s 
+JOIN Activity e
+    ON s.machine_id = e.machine_id 
+    AND s.process_id = e.process_id 
+    AND s.activity_type = 'start'
+    AND e.activity_type = 'end'
+GROUP BY s.machine_id;
