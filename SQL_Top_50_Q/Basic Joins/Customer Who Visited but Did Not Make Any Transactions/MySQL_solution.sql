dda7ab74330d8a3fb6cd97d5802e2ff98fa52904
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 12:26:15 -0400

    Update MySQL_solution.sql

--- SQL_Top_50_Q/Basic Joins/Customer Who Visited but Did Not Make Any Transactions/MySQL_solution.sql
+++ SQL_Top_50_Q/Basic Joins/Customer Who Visited but Did Not Make Any Transactions/MySQL_solution.sql
@@ -1,1 +1,18 @@
 -- MySQL solution for Customer Who Visited but Did Not Make Any Transactions
+
+-- Left Join version:
+SELECT V.customer_id, count(*) AS count_no_trans
+FROM Visits V LEFT JOIN Transactions T
+ON V.visit_id = T.visit_id
+WHERE T.transaction_id IS NULL
+GROUP BY V.customer_id
+
+-- Subquery version:
+SELECT customer_id, COUNT(*) as count_no_trans
+FROM Visits v
+WHERE (
+    SELECT COUNT(*) 
+    FROM Transactions t 
+    WHERE t.visit_id = v.visit_id
+) = 0
+GROUP BY customer_id;
