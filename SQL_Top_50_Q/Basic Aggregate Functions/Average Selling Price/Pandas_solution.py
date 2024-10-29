90f71acfa368a616459b87c0955aa3cd4929bf84
choppystick <93000287+choppystick@users.noreply.github.com>
2024-10-29 19:24:27 +0000

    uploaded solution for problem 1251

--- SQL_Top_50_Q/Basic Aggregate Functions/Average Selling Price/Pandas_solution.py
+++ SQL_Top_50_Q/Basic Aggregate Functions/Average Selling Price/Pandas_solution.py
@@ -1,1 +1,11 @@
 # Pandas solution for Average Selling Price
+import pandas as pd
+
+def average_selling_price(prices: pd.DataFrame, units_sold: pd.DataFrame) -> pd.DataFrame:
+    df = prices.merge(units_sold, how="left", on=["product_id"])
+    df = df.loc[(df['purchase_date'] >= df['start_date']) & (df['purchase_date'] <= df['end_date']) | (df['purchase_date'].isna())]
+    df["total"] = df['price'] * df["units"]
+    return df.fillna(0).groupby('product_id').agg(
+        {'total': 'sum',
+        'units': 'sum'}
+    ).apply(lambda x: round(x['total']/x['units'], 2) if x['units'] != 0 else 0, axis=1).reset_index(name="average_price")
