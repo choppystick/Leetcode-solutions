16de443497303426ef469b0565820cdd59293264
choppystick <93000287+choppystick@users.noreply.github.com>
2024-11-03 16:58:51 +0000

    uploaded solution for 1193

--- SQL_Top_50_Q/Basic Aggregate Functions/Monthly Transactions I/MsSQL_solution.sql
+++ SQL_Top_50_Q/Basic Aggregate Functions/Monthly Transactions I/MsSQL_solution.sql
@@ -1,1 +1,14 @@
-# MsSQL solution for Monthly Transactions I
+--  MsSQL solution for Monthly Transactions I
+
+SELECT 
+    FORMAT(trans_date, 'yyyy-MM') AS month, 
+    country,
+    COUNT(*) AS trans_count,
+    COUNT(CASE WHEN state = 'approved' THEN 1 ELSE NULL END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM 
+    Transactions
+GROUP BY 
+    FORMAT(trans_date, 'yyyy-MM'),  
+    country;
--- SQL_Top_50_Q/Basic Aggregate Functions/Monthly Transactions I/Pandas_solution.py
+++ SQL_Top_50_Q/Basic Aggregate Functions/Monthly Transactions I/Pandas_solution.py
@@ -1,1 +1,13 @@
 # Pandas solution for Monthly Transactions I
+import pandas as pd
+import numpy as np
+
+def monthly_transactions(transactions: pd.DataFrame) -> pd.DataFrame:
+    transactions['month'] = transactions['trans_date'].dt.strftime("%Y-%m")
+    transactions['state'] = np.where(transactions['state'] == 'approved', transactions['amount'], np.nan)
+    return transactions.groupby(['month', 'country'], dropna=False).agg(
+        trans_count=('id', 'count'),
+        approved_count=('state', 'count'),
+        trans_total_amount=('amount', 'sum'),
+        approved_total_amount=('state', 'sum')
+    ).reset_index()
